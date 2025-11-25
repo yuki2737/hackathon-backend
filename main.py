107062e07f4e0a0e80b036f9e10712f61a744e67
yuki2737 <youxiy377@gmail.com>
2025-11-25 23:50:18 +0900

    Add Dockerfile and requirements for Cloud Run

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+import os
+import pymysql
+
+app = FastAPI()
+
+def get_conn():
+    return pymysql.connect(
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PWD"),
+        host=os.getenv("MYSQL_HOST"),
+        database=os.getenv("MYSQL_DATABASE"),
+        charset="utf8mb4",
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+@app.get("/")
+def root():
+    return {"status": "Cloud Run working!"}
+
+@app.get("/test-db")
+def test_db():
+    try:
+        conn = get_conn()
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT NOW() AS time")
+            result = cursor.fetchone()
+        conn.close()
+        return {"message": "DB Connected!", "time": result["time"]}
+    except Exception as e:
+        return {"error": str(e)}
